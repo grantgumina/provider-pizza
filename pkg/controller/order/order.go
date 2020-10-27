3b8857a83ec4f3ea7e885a9147a834e645af8498
Grant Gumina <grant@Grants-MacBook-Pro.local>
2020-10-26 18:54:43 -0700

    trying to push to master

--- pkg/controller/order/order.go
+++ pkg/controller/order/order.go
@@ -1,8 +1,12 @@
 package order
 
 import (
+	"github.com/crossplane/crossplane-runtime/pkg/event"
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+	"github.com/grantgumina/provider-pizza/apis/order/v1alpha1"
+	apisv1alpha1 "github.com/grantgumina/provider-pizza/apis/v1alpha1"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
@@ -24,5 +28,15 @@ import (
 // Adds a controller which will reconcile Orders?
 func Setup(mgr ctrl.Manager, l logging.Logger) error {
 	// name := managed.ControllerName(v1alpha1.MyTypeGroupKind)
-	name := managed.ControllerName()
+	name := managed.ControllerName(v1alpha1.OrderKind)
+
+	r := managed.NewReconciler(mgr,
+		resource.ManagedKind(v1alpha1.OrderGroupVersionKind),
+		managed.WithExternalConnecter(&connector{
+			kube:         mgr.GetClient(),
+			usage:        resource.NewProviderConfigUsageTracker(mgr.GetClient(), &apisv1alpha1.ProviderConfigUsage{}),
+			newServiceFn: newNoOpService}),
+		managed.WithLogger(l.WithValues("controller", name)),
+		managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name))))
+
 }
